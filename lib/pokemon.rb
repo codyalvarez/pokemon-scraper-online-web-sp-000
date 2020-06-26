3cc45f76d2dbb4d883403cacfa66333404f1c141
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-26 00:17:24 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(id:, name: , type:)
+    @id = nil
+    @name = name
+    @type = type
+    @db = db
+
+  end
+
+  def self.save
+  end
+
+  def self.Find
+  end
 end
