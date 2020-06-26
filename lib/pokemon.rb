a67298f090e4916e9a5d048d1fe5105e47f2fe81
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-26 00:18:10 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,10 +7,15 @@ class Pokemon
     @name = name
     @type = type
     @db = db
-
   end
 
-  def self.save
+  def self.save(name, type)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.type)
   end
 
   def self.Find
